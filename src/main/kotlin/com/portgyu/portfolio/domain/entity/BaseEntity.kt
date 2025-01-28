b5a960e0283190087c4c1450d45f15435123ae5b
gyuminwoo <ug034516@gmail.com>
2025-01-28 18:55:21 +0900

    엔티티 개발 - 연관관계 없음 エンティティ開発‐関連関係なし

--- src/main/kotlin/com/portgyu/portfolio/domain/entity/BaseEntity.kt
+++ src/main/kotlin/com/portgyu/portfolio/domain/entity/BaseEntity.kt
@@ -1,7 +1,21 @@
 package com.portgyu.portfolio.domain.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
 
 @MappedSuperclass
 abstract class BaseEntity {
+
+    // null, update 방지
+    // CreatedDate와 LastModifiedDate는 세트 생성, 수정일
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
 }
