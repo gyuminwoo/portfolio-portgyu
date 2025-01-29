1113f1c6b634deef302e8c33df4d9af4c25f7899
gyuminwoo <ug034516@gmail.com>
2025-01-29 11:56:26 +0900

    리포지토리 성능 개선 リポジトリのパフォーマンス改善

--- src/main/kotlin/com/portgyu/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/portgyu/portfolio/domain/repository/ExperienceRepository.kt
@@ -1,9 +1,15 @@
 package com.portgyu.portfolio.domain.repository
 
 import com.portgyu.portfolio.domain.entity.Experience
-import com.portgyu.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.Optional
 
 interface ExperienceRepository:JpaRepository<Experience, Long> {
+
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
+    override fun findById(id: Long) :Optional<Experience>
 }
