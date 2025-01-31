1b13e20b6aa4e67da7a89f72ac0e4b857d192ab0
본가PC <ug034516@gmail.com>
2025-02-01 01:39:52 +0900

    삽입, 수정 API 개발 - 연관관계 없음 挿入・修正API開発‐関連関係なし

--- src/main/kotlin/com/portgyu/portfolio/admin/context/achievement/service/AdminAchievementService.kt
+++ src/main/kotlin/com/portgyu/portfolio/admin/context/achievement/service/AdminAchievementService.kt
@@ -1,9 +1,11 @@
 package com.portgyu.portfolio.admin.context.achievement.service
 
+import com.portgyu.portfolio.admin.context.achievement.form.AchievementForm
 import com.portgyu.portfolio.admin.data.TableDTO
 import com.portgyu.portfolio.domain.entity.Achievement
 import com.portgyu.portfolio.domain.repository.AchievementRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminAchievementService(
@@ -15,6 +17,18 @@ class AdminAchievementService(
         val entities = achievementRepository.findAll()
 
         return TableDTO.from(classInfo, entities)
+    }
+
+    @Transactional
+    fun save(form: AchievementForm) {
+        val achievement = form.toEntity()
+        achievementRepository.save(achievement)
+    }
+
+    @Transactional
+    fun update(id: Long, form: AchievementForm) {
+        val achievement = form.toEntity(id)
+        achievementRepository.save(achievement)
     }
 
 }
